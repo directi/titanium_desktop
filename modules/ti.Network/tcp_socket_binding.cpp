1e5f6d35088bf1122d57a18e0413478420962f77
Mital Vora <mital.d.vora@gmail.com>
2009-09-10 15:26:25 +0530

    Throwing error when connection is reset by peer

--- modules/ti.Network/tcp_socket_binding.cpp
+++ modules/ti.Network/tcp_socket_binding.cpp
@@ -168,14 +168,24 @@ namespace ti
 		catch(ValueException& e)
 		{
 			std::cerr << eprefix << *(e.GetValue()->DisplayString()) << std::endl;
+			ValueList args;
+			args.push_back(Value::NewString(*(e.GetValue()->DisplayString())));
+			ti_host->InvokeMethodOnMainThread(this->onError, args, false);
+			
 		}
 		catch(Poco::Exception &e)
 		{
 			std::cerr << eprefix << e.displayText() << std::endl;
+			ValueList args;
+			args.push_back(Value::NewString(e.displayText()));
+			ti_host->InvokeMethodOnMainThread(this->onError, args, false);
 		}
 		catch(...)
 		{
 			std::cerr << eprefix << "Unknown exception" << std::endl;
+			ValueList args;
+			args.push_back(Value::NewString("Unknown exception"));
+			ti_host->InvokeMethodOnMainThread(this->onError, args, false);
 		}
 	}
 	void TCPSocketBinding::OnWrite(const Poco::AutoPtr<WritableNotification>& n)
