2af67a0b235eda93f76b40cf48cc93b5deace433
Anirban Mitra <anirban.m@directi.com>
2009-12-28 21:28:40 +0530

    toolWindow should be true to show taskbar tab of window

--- modules/ti.App/window_config.cpp
+++ modules/ti.App/window_config.cpp
@@ -84,10 +84,7 @@ void WindowConfig::SetDefaults ()
 	this->resizable = true;
 
 	this->usingChrome = true;
-    // the default should not be true, as the default window should be
-    // be available in the taskbar. Setting a window as toolWindow, will remove
-    // the taskbar entry
-	this->toolWindow = false;
+	this->toolWindow = true;
 	this->usingScrollbars = true;
 	this->fullscreen = false;
 	this->maximized = false;
