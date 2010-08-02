0b9c5298e6ae7806bf6a83179edafe461c108e9b
Mital Vora <mital.d.vora@gmail.com>
2010-08-02 15:32:02 +0530

    Adding linux and mac files.. needs to verify actual functions with corresponding OS.

--- modules/ti.App/linux/linux_app_binding.cpp
+++ modules/ti.App/linux/linux_app_binding.cpp
@@ -30,4 +30,26 @@ void AppBinding::Restart(const ValueList& args, KValueRef result)
 	host->Exit(0);
 }
 
+void AppBinding::Exec(const ValueList& args, KValueRef result)
+{
+	if (args.size() > 0)
+	{
+		std::string command = args.at(0)->ToString();
+		std::wstring wcommand(command.begin(), command.end());
+		wchar_t *execargs[100];
+		int i = 0;
+		for(; i < args.size(); i++)
+		{
+			if(i == 99) // we limit commandline args to 100
+			{
+				break;
+			}
+			std::string param = args.at(i)->ToString();
+			std::wstring wparam(param.begin(), param.end());
+			execargs[i] = (wchar_t *)wparam.c_str();
+		}
+		execargs[i] = NULL;
+		//::_wexecvp(wcommand.c_str(), execargs);
+	}
+}
 }
