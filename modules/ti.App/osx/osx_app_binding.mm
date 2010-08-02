d77e1bc8ec46b59a800d41be311d7893b5b1c673
Mital Vora <mital@dv-mac-mital-v.internal.directi.com>
2010-08-02 17:29:28 +0530

    Finally Fixing in mac... now Titanium.App.exec() works on all three platforms

--- modules/ti.App/osx/osx_app_binding.mm
+++ modules/ti.App/osx/osx_app_binding.mm
@@ -36,9 +36,8 @@ void AppBinding::Exec(const ValueList& args, KValueRef result)
 	if (args.size() > 0)
 	{
 		std::string command = args.at(0)->ToString();
-		std::wstring wcommand(command.begin(), command.end());
-		wchar_t *execargs[100];
-		int i = 0;
+		char *execargs[100];
+		size_t i = 0;
 		for(; i < args.size(); i++)
 		{
 			if(i == 99) // we limit commandline args to 100
@@ -46,11 +45,10 @@ void AppBinding::Exec(const ValueList& args, KValueRef result)
 				break;
 			}
 			std::string param = args.at(i)->ToString();
-			std::wstring wparam(param.begin(), param.end());
-			execargs[i] = (wchar_t *)wparam.c_str();
+			execargs[i] = (char *)param.c_str();
 		}
 		execargs[i] = NULL;
-		//::_wexecvp(wcommand.c_str(), execargs);
+		execvp(command.c_str(), execargs);
 	}
 }
 }
