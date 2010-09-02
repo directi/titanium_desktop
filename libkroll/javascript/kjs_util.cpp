a93d5822e519354e259944987eb1e848e965ad51
Carl Lobo <carllobo@gmail.com>
2010-09-02 18:18:55 +0530

    Removed logger when the value is null and initialized return parameter to undefined before calling method.

--- libkroll/javascript/kjs_util.cpp
+++ libkroll/javascript/kjs_util.cpp
@@ -55,7 +55,6 @@ namespace KJSUtil
 
 		if (value == NULL)
 		{
-			GetLogger()->Error("Trying to convert NULL JSValueRef");
 			return Value::Undefined;
 		}
 
@@ -459,7 +458,7 @@ namespace KJSUtil
 			args.push_back(argVal);
 		}
 
-		JSValueRef jsValue = NULL;
+		JSValueRef jsValue = JSValueMakeUndefined(jsContext);
 		try
 		{
 			KValueRef kvalue = method->Call(args);
