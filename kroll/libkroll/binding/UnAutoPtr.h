6f25cd373745602549aceb27fe4cc3cdba107729
Carl Lobo <carllobo@gmail.com>
2010-11-16 18:14:23 +0530

    Adding a temp way to get rid of AutoPtrs... Incomplete.

---
+++ kroll/libkroll/binding/UnAutoPtr.h
@@ -0,0 +1,143 @@
+#ifndef BINDING_UN_AUTO_PTR_H
+#define BINDING_UN_AUTO_PTR_H
+
+namespace kroll {
+
+
+template <class C>
+class UnAutoPtr
+{
+public:
+	UnAutoPtr(): _ptr(0)
+	{
+	}
+
+	UnAutoPtr(C* ptr): _ptr(ptr)
+	{
+	}
+	
+
+	C* operator -> ()
+	{
+		return _ptr;
+	}
+
+	const C* operator -> () const
+	{
+		return _ptr;
+	}
+
+	C& operator * ()
+	{
+		return *_ptr;
+	}
+
+	const C& operator * () const
+	{
+		return *_ptr;
+	}
+
+	C* get()
+	{
+		return _ptr;
+	}
+
+	const C* get() const
+	{
+		return _ptr;
+	}
+
+	operator C* ()
+	{
+		return _ptr;
+	}
+	
+	operator const C* () const
+	{
+		return _ptr;
+	}
+	
+	bool operator ! () const
+	{
+		return _ptr == 0;
+	}
+
+	bool isNull() const
+	{
+		return _ptr == 0;
+	}
+	
+	template <class Other>
+	Other* cast() const
+	{
+		return dynamic_cast<Other*>(_ptr);
+	}
+
+	bool operator == (const C* ptr) const
+	{
+		return _ptr == ptr;
+	}
+
+	bool operator == (C* ptr) const
+	{
+		return _ptr == ptr;
+	}
+
+	bool operator != (const C* ptr) const
+	{
+		return _ptr != ptr;
+	}
+
+	bool operator != (C* ptr) const
+	{
+		return _ptr != ptr;
+	}
+
+	bool operator < (const C* ptr) const
+	{
+		return _ptr < ptr;
+	}
+
+	bool operator < (C* ptr) const
+	{
+		return _ptr < ptr;
+	}
+
+	bool operator <= (const C* ptr) const
+	{
+		return _ptr <= ptr;
+	}
+
+	bool operator <= (C* ptr) const
+	{
+		return _ptr <= ptr;
+	}
+
+	bool operator > (const C* ptr) const
+	{
+		return _ptr > ptr;
+	}
+
+	bool operator > (C* ptr) const
+	{
+		return _ptr > ptr;
+	}
+
+	bool operator >= (const C* ptr) const
+	{
+		return _ptr >= ptr;
+	}
+
+	bool operator >= (C* ptr) const
+	{
+		return _ptr >= ptr;
+	}
+
+private:
+	C* _ptr;
+};
+
+} 
+
+
+#endif // BINDING_UN_AUTO_PTR_H
