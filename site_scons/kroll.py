0cd4febd11a8d2a24b4c614623335b64a41dbf0c
Mital Vora <mital.d.vora@gmail.com>
2010-11-17 10:39:00 +0530

    Removing PocoXML dependency... two to go (PocoNet- which has already replaced by boost::asio and PocoFoundation :/

--- site_scons/kroll.py
+++ site_scons/kroll.py
@@ -172,7 +172,7 @@ class BuildConfig(object):
 		if name is 'poco':
 			cpppath = [self.tp('poco', 'include')]
 			libpath = [self.tp('poco', 'lib')]
-			libs = ['PocoFoundation', 'PocoNet', 'PocoXML']
+			libs = ['PocoFoundation', 'PocoNet']
 
 		if name is 'curl' and self.is_win32(): # Don't judge us!
 			cpppath = [self.tp('webkit', 'include')]
