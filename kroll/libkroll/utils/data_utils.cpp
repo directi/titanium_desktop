a96850be2931e17db221630d31d2294d1f847613
Mital Vora <mital.d.vora@gmail.com>
2010-11-23 15:15:20 +0530

    Removing: kashmir->the uuid generator.. using boost::uuids :) :)

---
+++ kroll/libkroll/utils/data_utils.cpp
@@ -0,0 +1,22 @@
+
+#include "data_utils.h"
+
+#include <sstream>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+
+namespace UTILS_NS
+{
+	namespace DataUtils
+	{
+		std::string GenerateUUID()
+		{
+			boost::uuids::random_generator generator;
+			boost::uuids::uuid newuuid(generator());
+			std::stringstream ss;
+			ss << newuuid;
+			return ss.str();
+		}
+	}
+}
